e4754a5e0fc86e686e5aa2ecd613b6fee57543ac
Wilfried AGO <wilfriedago@pm.me>
2025-04-04 21:58:47 +0100

    feat(db): add database helper and initialization logic
    - create a new DBHelper class for managing database connections
    - implement methods for initializing the database and creating tables
    - update main.dart to initialize the database on app startup
    - add necessary dependencies in pubspec.yaml

---
+++ lib/db/db.helper.dart
@@ -0,0 +1,70 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB();
+    return _database!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'budgetti.db');
+
+    print("Database path: $path");
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE budgets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        periodicite TEXT UNIQUE CHECK (periodicite IN ('hebdomadaire', 'mensuel', 'trimestriel', 'annuel')),
+        montant REAL NOT NULL,
+        created_at TEXT NOT NULL,
+        updated_at TEXT NOT NULL
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE depenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        categorie_id INTEGER,
+        montant REAL NOT NULL,
+        libelle TEXT NOT NULL,
+        observation TEXT,
+        FOREIGN KEY (categorie_id) REFERENCES categories(id)
+      )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE revenus (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT NOT NULL,
+        montant REAL NOT NULL,
+        libelle TEXT NOT NULL,
+        observation TEXT
+      )
+    ''');
+  }
+}

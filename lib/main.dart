704a8171e57567a98c9c35948ee8bc459c0517ad
ABOKAJR <marie.edmond.dossa@gmail.com>
2025-04-05 00:23:05 +0100

    suppression des fichiers non utilisés dans main.dart et changement de la couleur du seedColor au niveau du theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:budgetti/screens/categories/categories.screen.dart';
-import 'package:budgetti/screens/dashboard/dashboard.screen.dart';
-import 'package:budgetti/screens/expenses/expenses.screen.dart';
 import 'package:budgetti/screens/home.dart';
-import 'package:budgetti/screens/incomes/incomes.screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -19,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Mon Budget',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2962FF)),
       ),
       home: const HomePage(),
     );
